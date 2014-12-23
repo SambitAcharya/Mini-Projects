8c832b88cd5b9ce93826508504ebaf6bb20c2bd6
SambitAcharya <sambitacharya121@gmail.com>
2014-12-23 13:22:07 +0530

    Added a thresholding function to the image recognition file.

--- Python/Image Recognition With Python/imagerec.py
+++ Python/Image Recognition With Python/imagerec.py
@@ -1,9 +1,55 @@
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
-i = Image.open('images/dot.png')
+def threshold(imageArray):
+    balanceAr = []
+    newAr = imageArray
+
+    for eachRow in imageArray:
+        for eachPix in eachRow:
+            avgNum = reduce(lambda x,y: x + y, eachPix[:3]/len(eachPix[:3]))
+            balanceAr.append(avgNum)
+    balance = reduce(lambda x,y: x + y, balanceAr)/len(balanceAr)
+
+    for eachRow in newAr:
+        for eachPix in eachRow:
+            if (reduce(lambda x,y: x + y, eachPix[:3])/len(eachPix[:3]))> balance:
+                eachPix[0] = 255
+                eachPix[1] = 255
+                eachPix[2] = 255
+                eachPix[3] = 255
+            else:
+                eachPix[0] = 0
+                eachPix[1] = 0
+                eachPix[2] = 0
+                eachPix[3] = 255
+    return newAr
+
+i = Image.open('images/numbers/0.1.png')
 iar = np.asarray(i)
 
-plt.imshow(iar)
+i2 = Image.open('images/numbers/y0.4.png')
+iar2 = np.asarray(i2)
+
+i3 = Image.open('images/numbers/y0.5.png')
+iar3 = np.asarray(i3)
+
+i4 = Image.open('images/sentdex.png')
+iar4 = np.asarray(i4)
+
+threshold(iar3)
+
+fig = plt.figure()
+ax1 = plt.subplot2grid((8,6), (0,0), rowspan=4, colspan=3)
+ax2 = plt.subplot2grid((8,6), (4,0), rowspan=4, colspan=3)
+ax3 = plt.subplot2grid((8,6), (0,3), rowspan=4, colspan=3)
+ax4 = plt.subplot2grid((8,6), (4,3), rowspan=4, colspan=3)
+
+ax1.imshow(iar)
+ax2.imshow(iar2)
+ax3.imshow(iar3)
+ax4.imshow(iar4)
+
 plt.show()
