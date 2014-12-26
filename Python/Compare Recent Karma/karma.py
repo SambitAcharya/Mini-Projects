04d327b53e5f029fce3f1f0a306b8c589ac3a2f6
SambitAcharya <sambitacharya121@gmail.com>
2014-12-26 20:41:11 +0530

    Fixed a few bugs. Code is almost ready.

--- Python/Compare Recent Karma/karma.py
+++ Python/Compare Recent Karma/karma.py
@@ -2,31 +2,35 @@ import json
 import urllib2
 
 
-def obtainKarma(url):
+def obtainKarma(users_data):
 
-    data = json.load(user_data)
-    posts = data["data"]["children"]
-    number_of_posts = len(posts)
+    for user_data in users_data:
+        data = json.load(user_data)
+        posts = data["data"]["children"]
+        number_of_posts = len(posts)
 
-    scores = []
-    comments = []
+        scores = []
+        comments = []
 
-    for post_id in range(number_of_posts):
-        score = posts[post_id]["data"]["score"]
-        comment = posts[post_id]["data"]["num_comments"]
+        for post_id in range(number_of_posts):
+            score = posts[post_id]["data"]["score"]
+            comment = posts[post_id]["data"]["num_comments"]
 
-        scores.append(score)
-        comments.append(comment)
+            scores.append(score)
+            comments.append(comment)
 
-    print scores
-    print comments
+        print scores
+        print comments
 
 def getUserInfo():
 
-    while True:
+    users_data = []
+    count = 0
+    while count<2:
 
-        username = input("Enter the name of the user.\n")
-        
+        username = raw_input("Enter the name of the user. \n")
+        print username
+        count+=1
         url = "https://www.reddit.com/user/"+ username + ".json"
         print url
 
@@ -34,8 +38,18 @@ def getUserInfo():
             user_data = urllib2.urlopen(url)
         except urllib2.HTTPError, err:
             if err.code == 404:
-                print "This user doesn't exist."
+                print "This user doesn't exist.\nEnter another username."
+                count-=1
             else:
                 raise
 
-obtain_karma("pepsi_next")
+
+        users_data.append(user_data)
+
+    obtainKarma(users_data)
+
+
+def main():
+    getUserInfo()
+
+main()
