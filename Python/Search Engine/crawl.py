bff4f34393a5398bb396ab69a01d11fce8970900
Sambit Acharya <sambitacharya121@gmail.com>
2014-12-29 15:46:22 +0530

    Updated crawl.py

--- Python/Search Engine/crawl.py
+++ Python/Search Engine/crawl.py
@@ -1,7 +1,22 @@
 page =('<div id="top_bin"><div id="top_content" class="width960">'
 '<div class="udacity float-left"><a href="http://udacity.com">')
-start_link = page.find('<a href=')
-string=page[start_link:]
-start_link_2 = string.find('"') + 1
-url = string[start_link_2:-2]
-print url
+
+def get_next_target(page):
+    start_link = page.find('<a href=')
+    start_quote = page.find('"', start_link)
+    end_quote = page.find('"', start_quote+1)
+    url = page[start_quote+1: end_quote]
+    return url,end_quote
+
+
+
+def print_all_links(page):
+    while True:
+        url,end_pos = get_next_target(page)
+        if url:
+            print url
+            page = page[end_pos:]
+        else:
+            break
+
+print_all_links(page)
