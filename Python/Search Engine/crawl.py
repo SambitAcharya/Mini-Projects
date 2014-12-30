f1635695f792e7ab0c2c1c0807ba1e5537e1bcf1
Sambit Acharya <sambitacharya121@gmail.com>
2014-12-30 12:14:12 +0530

    Added a few more functions to the web crawler

--- Python/Search Engine/crawl.py
+++ Python/Search Engine/crawl.py
@@ -37,15 +37,36 @@ def union(a,b):
         if i not in a:
             a.append(i)
 
+def lookup(index,keyword):
+    for entry in index:
+        if entry[0] == keyword:
+            return entry[1]
+    return []
+
+def add_to_index(index,keyword,url):
+    for entry in index:
+        if entry[0] == keyword:
+            entry[1].append(url)
+            return
+    index.append([keyword,[url]])
+
+def add_page_to_index(index,url,content):
+    words = content.split()
+    for word in words:
+        add_to_index(index,word,url)
+
 def crawl_web(seed):
     tocrawl = [seed]
     crawled = []
+    index = []
     while tocrawl:
         page = tocrawl.pop()
         if page not in crawled:
-            union(tocrawl,get_all_links(get_page(page)))
+            content = get_page(page)
+            add_page_to_index(index,page,content)
+            union(tocrawl,get_all_links(content))
             crawled.append(page)
-    return crawled
+    return index
 
-links = crawl_web('http://xkcd.com/353')
-print links
+index = crawl_web('http://xkcd.com/353')
+print index
