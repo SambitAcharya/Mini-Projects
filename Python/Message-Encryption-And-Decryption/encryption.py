437bf0ba98e7f674e0ef66b2888cd5fb5e98af84
Sambit Acharya <sambitacharya121@gmail.com>
2014-12-31 11:25:07 +0530

    Updated encryption.py

--- Python/Message-Encryption-And-Decryption/encryption.py
+++ Python/Message-Encryption-And-Decryption/encryption.py
@@ -1,9 +1,52 @@
+'''
+
+Created For Mini Projects Repository
+
+Problem: Design an encryption program.
+
+Implementation: Python's encryption model PyCrypto has been used here. The program
+generates an encryption key and encrypts the message which is given by the user.
+The encryption key and the message are stored in their respective files so that
+they can be retrieved by the decryption file. Same key has been used for encryption
+and decryption.
+
+@author: Sambit
+
+'''
+
+#Imports
 from Crypto.Cipher import AES
 import base64
 import os
 
+#Functions
+
+#Function to write to a file.
+def fileWrite(filename,text):
+
+    '''
+
+        @param Input: The filename in which the given text is to be written
+        @none  Output: Text is written in the file.
+
+    '''
+
+
+    file = open(filename,'w')
+    file.write(text)
+    file.close
+
+#Function to encrypt a text.
 def encryption(privateInfo):
-    BLOCK_SIZE = 16
+
+    '''
+
+        @param Input: The message which is to be encrypted.
+        @print Output: The encrypted message.
+
+    '''
+
+    BLOCK_SIZE = 16  # 128 bit AES encryption(16 bytes)
     PADDING = '{'
 
     pad = lambda s: s+(BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
@@ -12,45 +55,13 @@ def encryption(privateInfo):
     secret = os.urandom(BLOCK_SIZE)
     print 'encryption key:', secret
 
-    file = open('key.txt','w')
-    file.write(secret)
-    file.close
+    fileWrite('key.txt',secret)
 
     cipher = AES.new(secret)
 
     encoded = EncodeAES(cipher,privateInfo)
 
-    file = open('message.txt','w')
-    file.write(encoded)
-    file.close
-
-    print 'Encrypted string:', encoded
-
-encryption('Test message for encryption. Hopefully it can be read only with the help of decryption file.')from Crypto.Cipher import AES
-import base64
-import os
-
-def encryption(privateInfo):
-    BLOCK_SIZE = 16
-    PADDING = '{'
-
-    pad = lambda s: s+(BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
-    EncodeAES = lambda c, s: base64.b64encode(c.encrypt(pad(s)))
-
-    secret = os.urandom(BLOCK_SIZE)
-    print 'encryption key:', secret
-
-    file = open('key.txt','w')
-    file.write(secret)
-    file.close
-
-    cipher = AES.new(secret)
-
-    encoded = EncodeAES(cipher,privateInfo)
-
-    file = open('message.txt','w')
-    file.write(encoded)
-    file.close
+    fileWrite('message.txt', encoded)
 
     print 'Encrypted string:', encoded
 
