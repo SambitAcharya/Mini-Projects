c1cbcbe3dfb927e9300c883c5890fab8348b050a
Sambit Acharya <sambitacharya121@gmail.com>
2015-01-03 21:26:09 +0530

    Created getdata.py

---
+++ Python/Decode-The-SMS/getdata.py
@@ -0,0 +1,38 @@
+'''
+
+Created for Mini-Projects Repository
+
+Problem: Getting the data to form a dictionary of SMS abbreviations
+
+Implementation: This scraper acquires data from http://www.webopedia.com/quick_ref/textmessageabbreviations.asp
+for the creation of the dictionary by using BeautifulSoup
+
+@author: Sambit
+
+'''
+
+#Imports
+import requests
+from bs4 import BeautifulSoup
+
+toappend = ""
+url = 'http://www.webopedia.com/quick_ref/textmessageabbreviations.asp'
+
+# Creating the Beautiful Soup object
+source_code = requests.get(url)
+html_text = source_code.text
+soup = BeautifulSoup(html_text)
+soup = soup.table.tbody
+
+#Iterating to get to the desired element
+for link in soup.findAll('tr'):
+    for abbr in link.findAll('td'):
+        for txt in abbr.findAll('p'):
+            toappend+=txt.text+" - "
+    toappend = toappend[:-2]
+    toappend+="\n"
+
+# Saving the data to a file for further processing
+abbreviations = open("abbreviations.txt",'w')
+abbreviations.write(toappend.encode('utf-8').strip())
+abbreviations.close()
