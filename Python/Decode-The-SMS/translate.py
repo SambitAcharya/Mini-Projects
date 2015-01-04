648d1da500a9f79fb8f5b3d3b90d9736a364dbc0
Sambit Acharya <sambitacharya121@gmail.com>
2015-01-04 10:53:24 +0530

    Updated translate.py

--- Python/Decode-The-SMS/translate.py
+++ Python/Decode-The-SMS/translate.py
@@ -1,1 +1,22 @@
  
+def createDictionary():
+
+    file_data = open("abbreviations.txt").read()
+    key_value = {}
+    data_list = file_data.split("\n")
+    del data_list[-1]
+
+    for data in data_list:
+        key_val = data.split("-")
+        key = key_val[0].strip()
+        value = key_val[1].strip()
+        key_value[key] = value
+
+    return key_value
+
+def main():
+    message = raw_input("Enter the message to be translated. \n")
+    
+    key_value = createDictionary()
+
+main()
