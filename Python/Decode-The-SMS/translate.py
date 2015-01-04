e8a193d4cc5327397ff0ee15c8b3d7413fbb3c22
Sambit Acharya <sambitacharya121@gmail.com>
2015-01-04 12:11:41 +0530

    Updated translate.py

--- Python/Decode-The-SMS/translate.py
+++ Python/Decode-The-SMS/translate.py
@@ -1,5 +1,25 @@
- 
+'''
+
+Created for Mini-Projects Repository
+
+Problem: Translating a SMS to a readable form for someone who has no idea about
+the latest SMS lingo.
+
+Implementation: A dictionary has been created from a file containing all the SMS
+abbreviations and this is used to translate the data which the user inputs.
+
+@author: Sambit
+
+'''
+
+# Function to create the dictionary
 def createDictionary():
+
+    '''
+
+        @return output: Dictionary created from the file
+
+    '''
 
     file_data = open("abbreviations.txt").read()
     key_value = {}
@@ -14,9 +34,51 @@ def createDictionary():
 
     return key_value
 
+# Function to translate the abbreviation if present.
+def translateAbbreviation(dictionary, abbreviation):
+
+    '''
+
+        @param Input: Dictionary created from the file and a word from the input message
+        @return Output: The translated word from the abbreviation if present in dictionary
+
+    '''
+
+    last_char = abbreviation[-1]
+    if last_char in ".?!,;:":
+        abbreviation = abbreviation.rstrip(last_char)
+    else:
+        last_char = ""
+
+    # translate abbreviation
+    if abbreviation in dictionary:
+        word = dictionary[abbreviation]
+    else:
+        word = abbreviation
+
+    return word + last_char
+
+# Function to translate the message words by word using translateAbbreviation function.
+def translateMessage(dictionary, message):
+
+    '''
+
+        @param Input: Dictionary created from the file and the input message
+        @return Output: The translated message
+
+    '''
+
+    translation = ""
+    for word in message.split():
+        translation += translateAbbreviation(dictionary, word) + " "
+    return translation
+
+#Main Function
 def main():
+
     message = raw_input("Enter the message to be translated. \n")
-    
     key_value = createDictionary()
+    print("The translated text is: ")
+    print(translateMessage(key_value, message))
 
 main()
