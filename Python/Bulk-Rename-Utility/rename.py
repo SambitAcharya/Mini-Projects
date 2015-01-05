927fce2afdf992912b779256bbd7c20860d2a334
Sambit Acharya <sambitacharya121@gmail.com>
2015-01-05 16:47:47 +0530

    Updated rename.py

--- Python/Bulk-Rename-Utility/rename.py
+++ Python/Bulk-Rename-Utility/rename.py
@@ -1,1 +1,44 @@
  
+import os
+import random
+
+def getFileList():
+
+    file_list = os.listdir(".")
+    return file_list
+
+def removeNumbers(file_list):
+
+    for file_name in file_list:
+        numbersremoved = ''.join(i for i in file_name if not i.isdigit())
+        os.rename(file_name,numbersremoved)
+
+def removeSymbols(file_list):
+
+    for file_name in file_list:
+        symbolsremoved = ''.join(e for e in file_name if (e.isalnum() or e=='.'))
+        os.rename(file_name,symbolsremoved)
+
+def removeCharacters(file_list,characters):
+
+    for file_name in file_list:
+
+        index = file_name.find('.')
+        ext = file_name[index:]
+        name = file_name[:index]
+        charactersremoved = ''.join(e for e in name if e not in characters)
+        charactersremoved+=ext
+        os.rename(file_name,charactersremoved)
+
+def getCharacterSet():
+
+    chars = raw_input("Enter the set of characters. \n")
+    char_list = chars.split()
+    return char_list
+
+def main():
+
+    file_list = getFileList()
+
+
+main()
