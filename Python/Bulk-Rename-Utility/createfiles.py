8dc88ae8101fa30cb59bfca6808d7b31505c44c6
Sambit Acharya <sambitacharya121@gmail.com>
2015-01-05 15:09:03 +0530

    Added createfiles.py

---
+++ Python/Bulk-Rename-Utility/createfiles.py
@@ -0,0 +1,30 @@
+def createFiles():
+
+    for i in range(100):
+
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+        filename = ""
+
+        for j in range(10):
+            next_letter_index = random.randrange(len(alphabet))
+            filename = filename + alphabet[next_letter_index]
+
+        filename = replaceWithNumber(filename)
+        filename = replaceWithUppercaseLetter(filename)
+
+        a = open(filename+'.txt','w')
+        a.close()
+
+def replaceWithNumber(fname):
+    for i in range(random.randrange(1,3)):
+        replace_index = random.randrange(len(fname)//2)
+        fname = fname[0:replace_index] + str(random.randrange(10)) + fname[replace_index+1:]
+        return fname
+
+# Function to replace 1 or 2 letters with an uppercase letter
+def replaceWithUppercaseLetter(fname):
+    for i in range(random.randrange(1,3)):
+        replace_index = random.randrange(len(fname)//2,len(fname))
+        fname = fname[0:replace_index] + fname[replace_index].upper() + fname[replace_index+1:]
+        return fname
