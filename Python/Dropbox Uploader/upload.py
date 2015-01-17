d6a4490bb77c3f246908dc337ecdfe1c1320f55b
SambitAcharya <sambitacharya121@gmail.com>
2015-01-18 01:04:47 +0530

    Added some more code.

--- Python/Dropbox Uploader/upload.py
+++ Python/Dropbox Uploader/upload.py
@@ -1,6 +1,21 @@
-#Including the dropbox sdk
+'''
+Created For Mini Projects Repository
+
+Problem: An application to upload files to dropbox.
+
+Implementation: The dropbox API is used to upload multiple files to the users
+dropbox account. A new directory called uploaded files is made in the cloud and
+all the files are uploaded there.
+
+@author: Sambit
+
+'''
+
+#Including the dropbox sdk and other required modules
 import dropbox
 import os
+import webbrowser
+
 
 #Providing the app key and app secret.
 app_key = 'nzy0jdjvmf8nf03'
@@ -9,12 +24,13 @@ app_secret = 'gmbfsgnz4zhec4e'
 flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
 
 authorize_url = flow.start()
+webbrowser.open(authorize_url)
 
 # Have the user sign in and authorize this token
-authorize_url = flow.start()
 print '1. Go to: ' + authorize_url
 print '2. Click "Allow" (you might have to log in first)'
 print '3. Copy the authorization code.'
+
 code = raw_input("Enter the authorization code here: ").strip()
 
 # This will fail if the user enters an invalid authorization code
