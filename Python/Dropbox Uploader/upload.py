b7fc1e5e75043fca9e4c18fdf63f4bf3f463c777
SambitAcharya <sambitacharya121@gmail.com>
2015-01-17 20:38:52 +0530

    Code added for obtaining user authorization.

--- Python/Dropbox Uploader/upload.py
+++ Python/Dropbox Uploader/upload.py
@@ -6,3 +6,12 @@ app_key = 'nzy0jdjvmf8nf03'
 app_secret = 'gmbfsgnz4zhec4e'
 
 flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
+
+authorize_url = flow.start()
+
+# Have the user sign in and authorize this token
+authorize_url = flow.start()
+print '1. Go to: ' + authorize_url
+print '2. Click "Allow" (you might have to log in first)'
+print '3. Copy the authorization code.'
+code = raw_input("Enter the authorization code here: ").strip()
