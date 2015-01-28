6e67cd993a54fc0be17f14b57c351b664553ccc0
SambitAcharya <sambitacharya121@gmail.com>
2015-01-28 19:33:22 +0530

    initial commit

--- Python/PyGithub/pygit.py
+++ Python/PyGithub/pygit.py
@@ -4,3 +4,17 @@ import json
 
 username = 'testZaccount'
 password = 'mytestaccount1234567890'
+
+url  = 'https://api.github.com/authorizations'
+note = 'My note'
+post_data = { 'scopes':['repo'],'note': note }
+
+response = requests.post(
+    url,
+    auth = (username, password),
+    data = json.dumps(post_data),
+    )
+
+print "API response:", response.text
+print
+print "Your OAuth token is", response.json()['token']
