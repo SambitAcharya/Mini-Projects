6ab2e9a22532d624943ca8e74679e5380db58ac6
SambitAcharya <sambitacharya121@gmail.com>
2015-02-02 20:56:43 +0530

    Added code to get properties of nodes and edges.

--- Python/PyGithub/pygit.py
+++ Python/PyGithub/pygit.py
@@ -8,3 +8,9 @@ g = nx.DiGraph()
 print "Nodes:", g.nodes()
 print "Edges:", g.edges()
 print
+
+print "X props:", g.node['X']
+print "Y props:", g.node['Y']
+
+print "X=>Y props:", g['X']['Y']
+print
