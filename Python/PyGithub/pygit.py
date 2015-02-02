4c0387e102d8cc6afdd2a3dd8ff498cd69f66dab
SambitAcharya <sambitacharya121@gmail.com>
2015-02-02 20:49:22 +0530

    Started network project.

--- Python/PyGithub/pygit.py
+++ Python/PyGithub/pygit.py
@@ -1,13 +1,3 @@
-from github import Github
+import networks as nx
 
-ACCESS_TOKEN = '5f569116bfbf651b435145d288bc35cb9cdf8b21'
-
-USER = 'ptwobrussell'
-REPO = 'Mining-the-Social-Web'
-
-client = Github(ACCESS_TOKEN, per_page=100)
-user = client.get_user(USER)
-repo = user.get_repo(REPO)
-
-stargazers = [ s for s in repo.get_stargazers() ]
-print "Number of stargazers", len(stargazers)
+g = nx.Digraph()
