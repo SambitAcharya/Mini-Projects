98eeb4de97379001288bca3452af7a89c4843e8b
SambitAcharya <sambitacharya121@gmail.com>
2015-02-10 00:20:32 +0530

    Started working on organization data.

--- Python/PyGithub/pygit.py
+++ Python/PyGithub/pygit.py
@@ -2,16 +2,21 @@ from github import Github
 
 ACCESS_TOKEN = 'YOUR_TOKEN_HERE'
 
+
 USER = 'SambitAcharya'
 REPO = 'Mini-Projects'
+ORG  = 'scipy'
 
 client = Github(ACCESS_TOKEN, per_page=100)
 
 user = client.get_user(USER)
 repo = user.get_repo(REPO)
 
-# print repo
-commits = [ s for s in repo.get_commits() ]
+organization = client.get_organization(ORG)
+repo_org = organization.get_repos()
+
+print repo_org
+# commits = [ s for s in repo.get_commits() ]
 # issues =  [ s for s in repo.get_issues() ]
 
-print "Number of commits", len(commits)
+# print "Number of commits:", len(commits)
