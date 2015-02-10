b531fd5085f81946d9fea8a8a1e88e27b0f58c45
SambitAcharya <sambitacharya121@gmail.com>
2015-02-11 01:03:40 +0530

    Added code to print connections data properly.

--- Python/LinkedIn Mining/linked.py
+++ Python/LinkedIn Mining/linked.py
@@ -1,10 +1,12 @@
 from linkedin import linkedin
 import json
+from prettytable import PrettyTable
 
 CONSUMER_KEY = 'YOUR-KEY-HERE'
 CONSUMER_SECRET = 'YOUR-SECRET-HERE'
 USER_TOKEN = 'YOUR-TOKEN-HERE'
 USER_SECRET = 'YOUR-SECRET-HERE'
+
 
 RETURN_URL = 'http://www.google.com'
 
@@ -15,8 +17,15 @@ app = linkedin.LinkedInApplication(auth)
 app.get_profile()
 
 connections = app.get_connections()
-connections_data = 'linkedin_connections.json'
+# connections_data = 'linkedin_connections.json'
+#
+# f = open(connections_data, 'w')
+# f.write(json.dumps(connections, indent=1))
+# f.close()
 
-f = open(connections_data, 'w')
-f.write(json.dumps(connections, indent=1))
-f.close()
+pt = PrettyTable(field_names=['Name', 'Location'])
+pt.align = 'l'
+
+[ pt.add_row((c['firstName'] + ' ' + c['lastName'], c['location']['name'])) for c in connections['values'] if c.has_key('location') ]
+
+print pt
