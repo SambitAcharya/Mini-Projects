1c2b602234ab3efedc7254cbe5a28aacb623b32e
SambitAcharya <sambitacharya121@gmail.com>
2015-02-10 22:36:15 +0530

    Added code for authentication.

--- Python/LinkedIn Mining/linked.py
+++ Python/LinkedIn Mining/linked.py
@@ -1,6 +1,14 @@
 from linkedin import linkedin
 
-# CONSUMER_KEY = 'YOUR-KEY-HERE'
-# CONSUMER_SECRET = 'YOUR-SECRET-HERE'
-# USER_TOKEN = 'YOUR-TOKEN-HERE'
-# USER_SECRET = 'YOUR-SECRET-HERE'
+CONSUMER_KEY = 'YOUR-KEY-HERE'
+CONSUMER_SECRET = 'YOUR-SECRET-HERE'
+USER_TOKEN = 'YOUR-TOKEN-HERE'
+USER_SECRET = 'YOUR-SECRET-HERE'
+
+RETURN_URL = 'http://www.google.com'
+
+auth = linkedin.LinkedInDeveloperAuthentication(CONSUMER_KEY, CONSUMER_SECRET,USER_TOKEN, USER_SECRET,RETURN_URL,permissions=linkedin.PERMISSIONS.enums.values())
+
+app = linkedin.LinkedInApplication(auth)
+
+app.get_profile()
