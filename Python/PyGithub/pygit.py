548f9b3d871148a7e3fd9424ddef2efda0024d87
SambitAcharya <sambitacharya121@gmail.com>
2015-02-16 03:33:33 +0530

    updated

--- Python/PyGithub/pygit.py
+++ Python/PyGithub/pygit.py
@@ -1,15 +1,16 @@
 from github import Github
+import numpy as np
 
 ACCESS_TOKEN = 'YOUR_TOKEN_HERE'
 
-USER = 'karan'
-REPO = 'Hook'
+USER = 'sambitacharya'
+# REPO = 'Hook'
 ORG  = 'scipy'
 
 client = Github(ACCESS_TOKEN, per_page=100)
 
 user = client.get_user(USER)
-repo = user.get_repo(REPO)
+# repo = user.get_repo(REPO)
 
 ## Code to get repo tags
 
@@ -23,16 +24,16 @@ repo = user.get_repo(REPO)
 
 ## Code to get repo watchers
 
-watchers = repo.get_watchers()
-
-print 'Hook by karan'
-count = 0
-
-for watcher in watchers:
-     print watcher.name
-     count+=1
-
-print count
+# watchers = repo.get_watchers()
+#
+# print 'Hook by karan'
+# count = 0
+#
+# for watcher in watchers:
+#      print watcher.name
+#      count+=1
+#
+# print count
 
 # Gives the list of stargazers.
 
@@ -50,25 +51,30 @@ print count
 ## Code to get repos, commits and stargazers
 
 # repos = user.get_repos()
-
+#
 # print "Repo","Commits","Stars"
 # print "-------------------"
 # for repo in repos:
 #
 #     name = repo.name
 #
-#     print name,
+#     # print name,
 #
 #     commits = [ s for s in repo.get_commits() ]
 #     commits_length = len(commits)
 #
-#     print commits_length,
+#     # print commits_length,
 #
 #     stargazers = [ s for s in repo.get_stargazers() ]
 #     stargazers_length = len(stargazers)
 #
-#     print stargazers_length
-
+#     # print stargazers_length
+#
+#     saveLine = name + ',' + str(commits_length) + ',' + str(stargazers_length) + '\n'
+#     print saveLine
+#     saveFile = open('newFile.csv','a')
+#     saveFile.write(saveLine)
+#
 ## End
 
 organization = client.get_organization(ORG)
