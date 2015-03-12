2c3dffc7129da0d2c34a76bfeb52e2a53118efa3
SambitAcharya <sambitacharya121@gmail.com>
2015-03-12 11:37:20 +0530

    Bar Plots

--- Python/Matplotlib/plot.py
+++ Python/Matplotlib/plot.py
@@ -1,19 +1,19 @@
 from matplotlib import pyplot as plt
 # from matplotlib import style
 
-x = [5,6,7,8]
+x = [1,3,5,7]
 y = [7,3,8,3]
 
-x2 = [5,6,7,8]
+x2 = [2,4,6,8]
 y2 = [6,7,2,6]
 
-plt.scatter(x,y,label = 'Line One')
+plt.bar(x,y,color = 'c')
 
-plt.scatter(x2,y2,label = 'Line Two')
+plt.bar(x2,y2,color = 'g')
 
-plt.title('Epic Chart')
-plt.ylabel('Y axis')
-plt.xlabel ('X axis')
+plt.title('Coder Profile')
+plt.ylabel('Percentile')
+plt.xlabel ('Languages')
 # plt.legend()
 
 plt.show()
