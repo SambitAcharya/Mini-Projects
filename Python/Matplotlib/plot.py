e06a07937cb16a850a8b7e44dd53a7c98c547773
SambitAcharya <sambitacharya121@gmail.com>
2015-03-12 11:30:51 +0530

    Scatter plots

--- Python/Matplotlib/plot.py
+++ Python/Matplotlib/plot.py
@@ -1,5 +1,5 @@
 from matplotlib import pyplot as plt
-from matplotlib import style
+# from matplotlib import style
 
 x = [5,6,7,8]
 y = [7,3,8,3]
@@ -7,11 +7,13 @@ y = [7,3,8,3]
 x2 = [5,6,7,8]
 y2 = [6,7,2,6]
 
-plt.plot(x,y,'g',linewidth = 5, label = 'Line One')
+plt.scatter(x,y,label = 'Line One')
 
-plt.plot(x2,y2,'c', linewidth = 10, label = 'Line Two')
+plt.scatter(x2,y2,label = 'Line Two')
 
 plt.title('Epic Chart')
 plt.ylabel('Y axis')
 plt.xlabel ('X axis')
-plt.legend()
+# plt.legend()
+
+plt.show()
