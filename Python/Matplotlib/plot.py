aaaec753fd7423329fbd48504306df8b9bf1e160
SambitAcharya <sambitacharya121@gmail.com>
2015-03-12 12:05:39 +0530

    Completed bar plot.

--- Python/Matplotlib/plot.py
+++ Python/Matplotlib/plot.py
@@ -2,15 +2,16 @@ from matplotlib import pyplot as plt
 # from matplotlib import style
 
 x = [1,3,5,7]
-y = [7,3,8,3]
+y = [70,30,80,30]
 
 x2 = [2,4,6,8]
-y2 = [6,7,2,6]
+y2 = [60,70,20,60]
 
-plt.bar(x,y,color = 'c')
+plt.bar(x,y,color = 'g', align = 'center')
 
-plt.bar(x2,y2,color = 'g')
+plt.bar(x2,y2,color = 'c', align = 'center')
 
+# plt.xticks([1000, 2000, 3000, 4000,5000, 6000, 7000, 8000,])
 plt.title('Coder Profile')
 plt.ylabel('Percentile')
 plt.xlabel ('Languages')
