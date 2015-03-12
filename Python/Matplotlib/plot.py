4f36fddb84ecbf231c6e0defff5d030e1df05952
SambitAcharya <sambitacharya121@gmail.com>
2015-03-12 12:24:32 +0530

    Plotting from CSV.

--- Python/Matplotlib/plot.py
+++ Python/Matplotlib/plot.py
@@ -1,20 +1,44 @@
 from matplotlib import pyplot as plt
 # from matplotlib import style
+import numpy as np
 
-x = [1,3,5,7]
-y = [70,30,80,30]
 
-x2 = [2,4,6,8]
-y2 = [60,70,20,60]
+'''
 
-plt.bar(x,y,color = 'g', align = 'center')
+Importing From CSV
 
-plt.bar(x2,y2,color = 'c', align = 'center')
 
-# plt.xticks([1000, 2000, 3000, 4000,5000, 6000, 7000, 8000,])
+
+x,y = np.loadtxt('examplefile.csv',unpack=True, delimiter = ',')
+
+plt.bar(x,y, color = 'g', align = 'center')
+
+
 plt.title('Coder Profile')
 plt.ylabel('Percentile')
 plt.xlabel ('Languages')
-# plt.legend()
 
+
+'''
+
+'''
+BAR Plots
+
+# # x = [1,3,5,7]
+# # y = [70,30,80,30]
+# #
+# # x2 = [2,4,6,8]
+# # y2 = [60,70,20,60]
+# #
+# # plt.bar(x,y,color = 'g', align = 'center')
+# #
+# # plt.bar(x2,y2,color = 'c', align = 'center')
+#
+# # plt.xticks([1000, 2000, 3000, 4000,5000, 6000, 7000, 8000,])
+# plt.title('Coder Profile')
+# plt.ylabel('Percentile')
+# plt.xlabel ('Languages')
+# # plt.legend()
+
+'''
 plt.show()
