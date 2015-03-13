102a00264cadf8feb5eeb704dff92ea82fd93d7a
SambitAcharya <sambitacharya121@gmail.com>
2015-03-13 16:09:17 +0530

    Added code to extract data to the html file.

--- Python/Extracting Data From PDF/extract.py
+++ Python/Extracting Data From PDF/extract.py
@@ -9,9 +9,8 @@ files = {'f':(filename+'.pdf',open(filename+'.pdf','rb'))}
 response = requests.post("https://pdftables.com/api?key=akp6nuf0ymip", files=files)
 response.raise_for_status()
 
-print response.text
-# for chunk in response.iter_content(chunk_size=1024):
-#     if chunk:
-#         htmlfile.write(chunk)
-#     htmlfile.flush()
-# htmlfile.close()
+for chunk in response.iter_content(chunk_size=1024):
+    if chunk:
+        htmlfile.write(chunk)
+    htmlfile.flush()
+htmlfile.close()
