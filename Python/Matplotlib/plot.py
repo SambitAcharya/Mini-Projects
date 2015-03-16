0805531cc5a835464d80d6b1cfe7a98c547c730a
SambitAcharya <sambitacharya121@gmail.com>
2015-03-16 17:12:58 +0530

    Updated the code.

--- Python/Matplotlib/plot.py
+++ Python/Matplotlib/plot.py
@@ -90,6 +90,17 @@ plt.plot(x,y)
 ax1 = fig.add_subplot(1,1,1, axisbg = 'grey')
 ax1.plot(x,y,'c', linewidth = 3.3)
 
+ax1.tick_params(axis  = 'x', colors = 'c')
+ax1.tick_params(axis  = 'y', colors = 'c')
+
+ax1.spines['bottom'].set_color('w')
+ax1.spines['top'].set_color('w')
+ax1.spines['left'].set_color('w')
+ax1.spines['right'].set_color('w')
+
+ax1.yaxis.label.set_color('c')
+ax1.xaxis.label.set_color('c')
+
 ax1.set_title('matplotlib title')
 ax1.set_title('xlabel')
 ax1.set_title('ylabel')
