843067eb724c04d81510b6269a3a453ca7165286
SambitAcharya <sambitacharya121@gmail.com>
2015-03-17 22:48:26 +0530

    Got the email and the domain name.

--- Python/Random Entry Selection From CSV/random_choice.py
+++ Python/Random Entry Selection From CSV/random_choice.py
@@ -6,7 +6,7 @@ import string
 def get_file_path(filename):
     currentdirpath = os.getcwd()
     file_path = os.path.join(os.getcwd(), filename)
-    print file_path
+    # print file_path
     return file_path
 
 def read_csv(filepath):
@@ -14,7 +14,8 @@ def read_csv(filepath):
         reader = csv.reader(csvfile)
         for row in reader:
             if not 'Email Address' in row:
-                print row[0], row[1], row[2]
+                email,domain = row[1].split('@')
+                print email,domain
 
 currentdirpath = os.getcwd()
 filename = 'choices.csv'
@@ -22,5 +23,5 @@ filename = 'choices.csv'
 #filepath to open
 file_path = os.path.join(os.getcwd(), filename)
 
-get_file_path('choices.csv')
+path = get_file_path('choices.csv')
 read_csv(path)
