3940c9c412b6e32fc8fe1fa263c6e28af942ca66
SambitAcharya <sambitacharya121@gmail.com>
2015-03-17 21:01:04 +0530

    Changed the order of the code.

--- Python/Random Entry Selection From CSV/random_choice.py
+++ Python/Random Entry Selection From CSV/random_choice.py
@@ -3,23 +3,24 @@ import csv
 import random
 import string
 
-currentdirpath = os.getcwd()
-filename = 'choices.csv'
-
-#filepath to open
-file_path = os.path.join(os.getcwd(), filename)
-
 def get_file_path(filename):
     currentdirpath = os.getcwd()
     file_path = os.path.join(os.getcwd(), filename)
     print file_path
     return file_path
 
-get_file_path('choices.csv')
-
 def read_csv(filepath):
     with open(filepath, 'rU') as csvfile:
         reader = csv.reader(csvfile)
-        print reader
+        for row in reader:
+            if not 'Email Address' in row:
+                print row[0], row[1], row[2]
 
+currentdirpath = os.getcwd()
+filename = 'choices.csv'
+
+#filepath to open
+file_path = os.path.join(os.getcwd(), filename)
+
+get_file_path('choices.csv')
 read_csv(path)
