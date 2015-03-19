27fba8cb3995a64ad8cbe55248ca80b8e133de18
SambitAcharya <sambitacharya121@gmail.com>
2015-03-19 12:28:55 +0530

    Code to get article urls and category urls

--- Python/Collect The Articles/collect.py
+++ Python/Collect The Articles/collect.py
@@ -1,1 +1,15 @@
-from newspaper import Article
+import newspaper
+
+cnn_paper = newspaper.build('http://cnn.com')
+
+art = []
+cat = []
+
+for article in cnn_paper.articles:
+    art.append(article.url)
+
+for category in cnn_paper.category_urls():
+    cat.append(category)
+
+print len(art)
+print len(cat)
