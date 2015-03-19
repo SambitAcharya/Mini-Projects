cc2db26859de5652a28288e1002245564c2bc10c
SambitAcharya <sambitacharya121@gmail.com>
2015-03-19 14:49:56 +0530

    articles and authors.

--- Python/Collect The Articles/collect.py
+++ Python/Collect The Articles/collect.py
@@ -5,11 +5,18 @@ cnn_paper = newspaper.build('http://cnn.com')
 art = []
 cat = []
 
-for article in cnn_paper.articles:
-    art.append(article.url)
+# for article in cnn_paper.articles:
+#     art.append(article.url)
+#
+# for category in cnn_paper.category_urls():
+#     cat.append(category)
 
-for category in cnn_paper.category_urls():
-    cat.append(category)
-
-print len(art)
-print len(cat)
+article = cnn_paper.articles[0]
+article.download()
+article.parse()
+# html = article.html
+text = article.text
+authors = article.authors
+# print html
+print authors
+print text
