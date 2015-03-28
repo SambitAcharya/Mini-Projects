6df148a8a32580c205dc3b6ed4b6173894210a57
SambitAcharya <sambitacharya121@gmail.com>
2015-03-28 15:27:52 +0530

    More of the above.

--- Python/Scrape Euler/scraper.py
+++ Python/Scrape Euler/scraper.py
@@ -4,8 +4,6 @@ import re
 path = 'ksharma377'
 img = Image.open(path+'.png')
 c=img.crop((5,40,75,60))
-# c=img.crop((45,40,75,60))
-# c.show()
 a = pytesseract.image_to_string(c)
 a = re.sub("[^0-9]", "", a)
 
