81a1ad26d0a3c22654680376b6321aabbf0d41fd
SambitAcharya <sambitacharya121@gmail.com>
2015-03-28 15:24:13 +0530

    Removed code to convert to jpeg.

--- Python/Scrape Euler/scraper.py
+++ Python/Scrape Euler/scraper.py
@@ -7,8 +7,6 @@ import re
 # path = r'/Users/Sambit/Mini-Projects/Python/Scrape Euler/praveen97uma.png'
 path = 'ksharma377'
 img = Image.open(path+'.png')
-img.save(path+'.jpg','JPEG')
-img.save(path+'.jpg',quality=200)
 c=img.crop((5,40,75,60))
 # c=img.crop((45,40,75,60))
 # c.show()
@@ -16,4 +14,3 @@ a = pytesseract.image_to_string(c)
 a = re.sub("[^0-9]", "", a)
 
 print a
- 
