281ec2bb0bc719776df36fc328ad4cde1f342304
SambitAcharya <sambitacharya121@gmail.com>
2015-03-28 20:14:23 +0530

    Added comments.

--- Python/Scrape Euler/scraper.py
+++ Python/Scrape Euler/scraper.py
@@ -1,3 +1,4 @@
+# Imports
 from PIL import Image
 import argparse
 import pytesseract
@@ -7,29 +8,45 @@ import httplib
 import os
 
 def getEulerScore(username):
+
+    '''
+
+        Function to get the euler score, accepting the username
+        as a parameter.
+
+    '''
+
     url = 'https://projecteuler.net/profile/'+username+'.png'
-    # c = httplib.HTTPConnection(url)
-    # c.request("HEAD",'')
-    # if c.getresponse().status == 200:
-    #     return True
-    # else:
-    #     return False
-    # filename = url.split('/')[-1]
-    # image = urllib.urlretrieve(url, filename)
-    # img = Image.open(filename)
-    # c=img.crop((5,40,75,60))
-    # score = pytesseract.image_to_string(c)
-    # score = re.sub("[^0-9]", "", score)
-    # os.remove(filename)
-    # return score
+    filename = url.split('/')[-1]
+
+    image = urllib.urlretrieve(url, filename) # Downloading the image from projecteuler site.
+    img = Image.open(filename)
+
+    cropped_image=img.crop((5,40,75,60)) # Cropping the image for better clarity
+
+    score = pytesseract.image_to_string(cropped_image) # Converting image to text using the library
+
+    score = re.sub("[^0-9]", "", score) # Removing all non digit characters from the extracted text.
+    os.remove(filename) # Deleting the downloaded image to save space
+
+    return score
 
 
 def main():
+
+    '''
+
+        Main Function
+
+    '''
+
     parser = argparse.ArgumentParser()
     parser.add_argument('name', help='Username of the Eulerian',type = str)
     args = parser.parse_args()
+
     username =  args.name
     score = getEulerScore(username)
+
     print score
 
 if __name__ == '__main__':
