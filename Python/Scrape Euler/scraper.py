d9be1488ebd346cb0e1323105853710ded270f4b
SambitAcharya <sambitacharya121@gmail.com>
2015-03-28 15:27:03 +0530

    Removed testing comments.

--- Python/Scrape Euler/scraper.py
+++ Python/Scrape Euler/scraper.py
@@ -1,10 +1,6 @@
-# try:
-#     import Image
-# except ImportError:
 from PIL import Image
 import pytesseract
 import re
-# path = r'/Users/Sambit/Mini-Projects/Python/Scrape Euler/praveen97uma.png'
 path = 'ksharma377'
 img = Image.open(path+'.png')
 c=img.crop((5,40,75,60))
