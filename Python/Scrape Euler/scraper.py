23b0292c8e585fb94a1578f9fea1548e06025704
SambitAcharya <sambitacharya121@gmail.com>
2015-03-28 19:54:02 +0530

    Check for bad pages.

--- Python/Scrape Euler/scraper.py
+++ Python/Scrape Euler/scraper.py
@@ -3,18 +3,25 @@ import argparse
 import pytesseract
 import re
 import urllib
+import httplib
 import os
 
 def getEulerScore(username):
     url = 'https://projecteuler.net/profile/'+username+'.png'
-    filename = url.split('/')[-1]
-    image = urllib.urlretrieve(url, filename)
-    img = Image.open(filename)
-    c=img.crop((5,40,75,60))
-    score = pytesseract.image_to_string(c)
-    score = re.sub("[^0-9]", "", score)
-    os.remove(filename)
-    return score
+    # c = httplib.HTTPConnection(url)
+    # c.request("HEAD",'')
+    # if c.getresponse().status == 200:
+    #     return True
+    # else:
+    #     return False
+    # filename = url.split('/')[-1]
+    # image = urllib.urlretrieve(url, filename)
+    # img = Image.open(filename)
+    # c=img.crop((5,40,75,60))
+    # score = pytesseract.image_to_string(c)
+    # score = re.sub("[^0-9]", "", score)
+    # os.remove(filename)
+    # return score
 
 
 def main():
