052233ec864c750726e7e7493622e7f93967b142
SambitAcharya <sambitacharya121@gmail.com>
2015-03-29 18:57:22 +0530

    Code for getting industry data.

--- Python/Linkedin Company Directory/scrapecompany.py
+++ Python/Linkedin Company Directory/scrapecompany.py
@@ -1,2 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
+
+def getIndustryData():
+
+    url = 'https://www.linkedin.com/directory/companies/'
+    source_code = requests.get(url)
+    html_text = source_code.text
+    soup = BeautifulSoup(html_text)
+    urls = []
+    for item in soup.findAll('li',{"class":"content"}):
+        link = item.find('a')
+        url  = link.get('href')
+        urls.append(url)
+
+
+getIndustryData()
