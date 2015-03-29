a61995e5070d91dbf7836a620967168447dfa17f
SambitAcharya <sambitacharya121@gmail.com>
2015-03-29 19:14:31 +0530

    Added codefor location.

--- Python/Linkedin Company Directory/scrapecompany.py
+++ Python/Linkedin Company Directory/scrapecompany.py
@@ -1,17 +1,37 @@
 from bs4 import BeautifulSoup
 import requests
 
-def getIndustryData():
+def getSoupObject(url):
 
-    url = 'https://www.linkedin.com/directory/companies/'
     source_code = requests.get(url)
     html_text = source_code.text
     soup = BeautifulSoup(html_text)
+
+    return soup
+
+def getIndustryDataByPlace(urls,names):
+
+    for index,url in enumerate(urls):
+        print names[index]
+        print url
+        print
+
+def getDataByIndustry():
+
+    url = 'https://www.linkedin.com/directory/companies/'
+    soup = getSoupObject(url)
+
     urls = []
+    names = []
+
     for item in soup.findAll('li',{"class":"content"}):
+        name = item.string
+        names.append(name)
         link = item.find('a')
         url  = link.get('href')
         urls.append(url)
 
+    getIndustryDataByPlace(urls,names)
 
-getIndustryData()
+
+getDataByIndustry()
