0f09f544524d4c0b87d020ba1b61c959c531fb63
SambitAcharya <sambitacharya121@gmail.com>
2015-03-31 03:37:52 +0530

    Updated the code.

--- Python/Linkedin Company Directory/scrapecompany.py
+++ Python/Linkedin Company Directory/scrapecompany.py
@@ -18,88 +18,50 @@ def getUrlsFromPage(url):
 
     for item in soup.findAll('li',{"class":"content"}):
         name = item.string
-        # print name
         names.append(name)
         link = item.find('a')
         url  = link.get('href')
-        # print url
         urls.append(url)
 
     return urls
 
-def getNameAndLogo(urls):
-
-    for url in urls:
-        # print url
-        soup = getSoupObject(url)
-        print soup
-        # title = soup.find('img',{'class':'image'})
-        # print title.get(src)
-
 def getCompaniesByPlace(urls):
 
-    # urls = getNameAndUrl(urls)
-
-    # print urls
-
     for index,url in enumerate(urls):
-        # soup = getSoupObject(url)
-        #
-        # urls  = []
-        # names = []
-        #
-        # for item in soup.findAll('li',{"class":"content"}):
-        #     name = item.string
-        #     print name
-        #     names.append(name)
-        #     link = item.find('a')
-        #     url  = link.get('href')
-        #     print url
-        #     urls.append(url)
-        # print '\n'*10
-        urls = getUrlsFromPage(url)
-        getNameAndLogo(urls)
+        soup = getSoupObject(url)
+
+        urls  = []
+        names = []
+
+        for item in soup.findAll('li',{"class":"content"}):
+            name = item.string
+            names.append(name)
+            link = item.find('a')
+            url  = link.get('href')
+            urls.append(url)
+            string = name + ' - ' + url + '\n'
+            print string
+            file.write(string.encode('utf8'))
+        
 
 def getIndustryDataByPlace(urls):
 
     for index,url in enumerate(urls):
-        # soup = getSoupObject(url)
-        #
-        # urls  = []
-        # names = []
-        #
-        # for item in soup.findAll('li',{"class":"content"}):
-        #     name = item.string
-        #     # print name
-        #     names.append(name)
-        #     link = item.find('a')
-        #     url  = link.get('href')
-        #     # print url
-        #     urls.append(url)
-
-
         urls = getUrlsFromPage(url)
-
         getCompaniesByPlace(urls)
 
 
 def getDataByIndustry():
 
     url  = 'https://www.linkedin.com/directory/companies/'
-    # soup = getSoupObject(url)
-    #
-    # urls  = []
-    # names = []
-    #
-    # for item in soup.findAll('li',{"class":"content"}):
-    #     name = item.string
-    #     names.append(name)
-    #     link = item.find('a')
-    #     url  = link.get('href')
-    #     urls.append(url)
     urls = getUrlsFromPage(url)
 
     getIndustryDataByPlace(urls)
 
+def main():
+    
+    getDataByIndustry()
 
-getDataByIndustry()
+file = open('companies2.txt','a')
+if __name__ == '__main__':
+    main()
